6ee7defb75d21c17e2caf8f29f3d586631d3851d
wismna <geogeob@hotmail.com>
2017-12-22 15:51:27 +0100

    Update DynamicDialogFragment.java

--- fragments/DynamicDialogFragment.java
+++ fragments/DynamicDialogFragment.java
@@ -1,14 +1,16 @@
 package com.wismna.geoffroy.donext.fragments;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -21,8 +23,6 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.FrameLayout;
 
-import com.wismna.geoffroy.donext.R;
-
 /**
  * Created by wismna on 2017-03-21.
  * Sub-class this class to create a dynamic fragment that will act as a Dialog in large layouts and
@@ -30,16 +30,17 @@ import com.wismna.geoffroy.donext.R;
  */
 
 public abstract class DynamicDialogFragment extends DialogFragment {
-    boolean mHasNeutralButton = false;
+    int mButtonCount = 2;
     int mContentLayoutId = 0;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // This part is only needed on small layouts (large layouts use onCreateDialog)
         if (!getShowsDialog()) {
             View view = inflater.inflate(R.layout.fragment_dynamic_dialog, container, false);
             AppCompatActivity activity = (AppCompatActivity) getActivity();
+            assert activity != null;
             activity.setSupportActionBar(setToolbarTitle(view));
 
             ActionBar actionBar = activity.getSupportActionBar();
@@ -60,19 +61,17 @@ public abstract class DynamicDialogFragment extends DialogFragment {
     @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Inflate and set the layout for the dialog
-        LayoutInflater inflater = getActivity().getLayoutInflater();
+        Activity activity = getActivity();
+        assert activity != null;
+        LayoutInflater inflater = activity.getLayoutInflater();
         // As it is a Dialog, the root ViewGroup can be null without issues
-        @SuppressLint("InflateParams") final View view = inflater.inflate(R.layout.fragment_dynamic_dialog, null);
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final View view = inflater.inflate(R.layout.fragment_dynamic_dialog, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         Bundle args = getArguments();
         // Set the dialog buttons
+        assert args != null;
+        // Add action buttons
         builder.setView(view)
-                // Add action buttons
-                .setPositiveButton(args.getString("button_positive"), new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        onPositiveButtonClick(view);
-                    }
-                })
                 .setNegativeButton(args.getString("button_negative"), new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // Send the negative button event back to the host activity
@@ -81,7 +80,14 @@ public abstract class DynamicDialogFragment extends DialogFragment {
                         onNegativeButtonClick();
                     }
                 });
-        if (mHasNeutralButton) {
+        if (mButtonCount == 2) {
+            builder.setPositiveButton(args.getString("button_positive"), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    onPositiveButtonClick(view);
+                }
+            });
+        }
+        if (mButtonCount == 3) {
             builder.setNeutralButton(args.getString("button_neutral"), new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
@@ -96,7 +102,6 @@ public abstract class DynamicDialogFragment extends DialogFragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        //super.onCreateOptionsMenu(menu, inflater);
         menu.clear();
         getActivity().getMenuInflater().inflate(R.menu.menu_dynamic_fragment, menu);
     }
@@ -104,14 +109,22 @@ public abstract class DynamicDialogFragment extends DialogFragment {
     @Override
     public void onPrepareOptionsMenu(Menu menu) {
         Bundle args = getArguments();
+        assert args != null;
         // Show the neutral button if needed
-        if (!mHasNeutralButton) {
+        if (mButtonCount < 3) {
             menu.removeItem(R.id.menu_neutral_button);
         }
         else {
             menu.findItem(R.id.menu_neutral_button).setTitle(args.getString("button_neutral"));
         }
-        menu.findItem(R.id.menu_positive_button).setTitle(args.getString("button_positive"));
+
+        // Show the positive button if needed
+        if (mButtonCount < 2) {
+            menu.removeItem(R.id.menu_positive_button);
+        }
+        else {
+            menu.findItem(R.id.menu_positive_button).setTitle(args.getString("button_positive"));
+        }
         super.onPrepareOptionsMenu(menu);
     }
 
@@ -124,6 +137,7 @@ public abstract class DynamicDialogFragment extends DialogFragment {
         // Hide the keyboard if present
         if (view != null) {
             InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
+            assert imm != null;
             imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
         }
         if (id == R.id.menu_positive_button) {
@@ -156,15 +170,31 @@ public abstract class DynamicDialogFragment extends DialogFragment {
         super.onDestroyView();
     }
 
+    public void showFragment(FragmentManager fragmentManager, String title, boolean isLargeLayout)
+    {
+        if (isLargeLayout)
+            show(fragmentManager, title);
+        else {
+            // The device is smaller, so show the fragment fullscreen
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            // For a little polish, specify a transition animation
+            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            // To make it fullscreen, use the 'content' root view as the container
+            // for the fragment, which is always the root view for the activity
+            transaction.add(android.R.id.content, this, title)
+                    .addToBackStack(null).commit();
+        }
+    }
+
     /** Helper function to get a View, without having to worry about the fact that is a Dialog or not*/
-    protected View findViewById(int id) {
+    protected <T extends View> T findViewById(int id) {
         if (getShowsDialog()) return getDialog().findViewById(id);
         return getView().findViewById(id);
     }
 
     /** Sets the title of the Fragment from the Tag */
     private Toolbar setToolbarTitle(View view) {
-        Toolbar toolbar = (Toolbar) view.findViewById(R.id.dialog_toolbar);
+        Toolbar toolbar = view.findViewById(R.id.dialog_toolbar);
         toolbar.setTitle(getTag());
         return toolbar;
     }
@@ -174,7 +204,7 @@ public abstract class DynamicDialogFragment extends DialogFragment {
         // Ensure that the content view is set
         if (mContentLayoutId == 0) return;
         // Insert the content view
-        FrameLayout content = (FrameLayout) view.findViewById(R.id.dynamic_fragment_content);
+        FrameLayout content = view.findViewById(R.id.dynamic_fragment_content);
         content.addView(inflater.inflate(mContentLayoutId, (ViewGroup) view.getParent()));
     }
 
