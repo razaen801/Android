bc881ee2bd79d18a4b2f1e74ccec3993b64c5e81
wismna <geogeob@hotmail.com>
2017-12-22 17:37:41 +0100

    Update DynamicDialogFragment.java

--- fragments/DynamicDialogFragment.java
+++ fragments/DynamicDialogFragment.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
